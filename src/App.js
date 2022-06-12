83b4f79b2bbacd406bbc3b53435773c49c91560b
Shivakumar D N <shivakumardn@Shivakumars-MacBook-Pro.local>
2022-06-12 13:40:15 +0530

    first AWS deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First AWS deploy
         </a>
       </header>
     </div>
